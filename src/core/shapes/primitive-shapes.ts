382a749c6a62f8e1b16dbe7a2e68259f152509e5
Miriam Zusin <miriamzusin@gmail.com>
2023-04-23 16:05:05 +0300

    path

--- src/core/shapes/primitive-shapes.ts
+++ src/core/shapes/primitive-shapes.ts
@@ -1,4 +1,5 @@
 import { fill, stroke } from '../canvas';
+import { Vector2 } from 'mz-math/types/types';
 
 export interface IRectProps {
     x: number;
@@ -49,4 +50,45 @@ export const rect = (ctx: CanvasRenderingContext2D, props: IRectProps) => {
         stroke(ctx, props);
         ctx.strokeRect(x, y, w, h);
     }
+};
+
+export interface IPathProps {
+
+    points: Vector2[];
+
+    fillStyle?: string;
+    strokeStyle?: string;
+    lineWidth?: number;
+}
+
+export const path = (ctx: CanvasRenderingContext2D, props: IPathProps) => {
+
+    const { points } = props;
+
+    fill(ctx, props);
+    stroke(ctx, props);
+
+    ctx.beginPath();
+
+    for(let i= 0; i<points.length; i++) {
+        const point = points[i];
+
+        if(i === 0){
+            ctx.moveTo(point[0], point[1]);
+        }
+        else{
+            ctx.lineTo(point[0], point[1]);
+        }
+    }
+
+    ctx.closePath();
+
+    if(props.fillStyle){
+        ctx.fill();
+    }
+
+    if(props.strokeStyle){
+        ctx.stroke();
+    }
+
 };
