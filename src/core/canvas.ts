b948ba9fe14de9d96235dbe493c07caa3d4d002e
Miriam Zusin <miriamzusin@gmail.com>
2023-04-23 22:26:54 +0300

    colors and styles

--- src/core/canvas.ts
+++ src/core/canvas.ts
@@ -11,6 +11,17 @@ const setAttributes = ($canvas: HTMLCanvasElement, attributes: [string, string|n
     }
 };
 
+const setContextProps = (props: [string, string|number|undefined][], ctx: CanvasRenderingContext2D) => {
+    for(const prop of props){
+
+        const value = prop[1];
+        if(value === undefined) continue;
+
+        const key = prop[0];
+        ctx[key] = value;
+    }
+};
+
 export const canvas = (props: ICanvas) => {
 
     const $canvas: HTMLCanvasElement = document.createElement('canvas');
@@ -24,6 +35,7 @@ export const canvas = (props: ICanvas) => {
         ['title', props.title],
         ['tabindex', props.tabindex],
         ['role', props.role],
+        ['aria-label', props.ariaLabel],
     ]);
 
     // Used to make the canvas accessible by providing fallback text
@@ -39,17 +51,30 @@ export const canvas = (props: ICanvas) => {
 };
 
 export const stroke = (props: IStrokeProps, ctx: CanvasRenderingContext2D) => {
-    if(props.lineWidth){
-        ctx.lineWidth = props.lineWidth;
-    }
+    setContextProps([
+        ['lineWidth', props.lineWidth],
+        ['strokeStyle', props.strokeStyle],
+        ['lineCap', props.lineCap],
+        ['lineJoin', props.lineJoin],
+        ['miterLimit', props.miterLimit],
+        ['lineDashOffset', props.lineDashOffset],
+    ],ctx);
 
-    if(props.strokeStyle){
-        ctx.strokeStyle = props.strokeStyle;
+    if(props.lineDashSegments){
+        ctx.setLineDash(props.lineDashSegments);
     }
 };
 
 export const fill = (props: IFillProps, ctx: CanvasRenderingContext2D) => {
-    if(props.fillStyle){
+
+    if(props.fillStyle !== undefined){
         ctx.fillStyle = props.fillStyle;
     }
+
+    setContextProps([
+        ['shadowOffsetX', props.shadowOffsetX],
+        ['shadowOffsetY', props.shadowOffsetY],
+        ['shadowBlur', props.shadowBlur],
+        ['shadowColor', props.shadowColor],
+    ],ctx);
 };
--- src/interfaces.ts
+++ src/interfaces.ts
@@ -13,15 +13,74 @@ export interface ICanvas {
     // accessibility --------
     role?: string;
     fallback?: string;
+    ariaLabel?: string;
 }
 
+/**
+ * https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Applying_styles_and_colors#line_styles
+ */
 export interface IStrokeProps {
+
+    // Line color
     strokeStyle?: string;
+
+    // Sets the width of lines drawn in the future.
     lineWidth?: number;
+
+    // Sets the appearance of the ends of lines.
+    // "butt", "round", "square". The default is "butt".
+    lineCap?: string;
+
+    // Sets the appearance of the "corners" where lines meet.
+    // "round", "bevel", and "miter". The default is "miter".
+    lineJoin?: string;
+
+    // Establishes a limit on the miter when two lines join at a sharp angle,
+    // to let you control how thick the junction becomes.
+    // Zero, negative, Infinity, and NaN values are ignored.
+    // The default value is 10.0.
+    miterLimit?: number;
+
+    // Used in setLineDash() that defines the current line dash pattern.
+    // For example, [5, 15]
+    lineDashSegments: number[];
+
+    // Specifies where to start a dash array on a line.
+    // The default value is 0.
+    lineDashOffset?: number;
 }
 
+/**
+ * https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Applying_styles_and_colors#gradients
+ * https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Applying_styles_and_colors#patterns
+ * https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Applying_styles_and_colors#shadows
+ */
 export interface IFillProps {
-    fillStyle?: string;
+    fillStyle?: string|CanvasGradient|CanvasPattern;
+
+    // How far the shadow should extend from the object in the X direction.
+    // This value isn't affected by the transformation matrix.
+    // Use negative values to cause the shadow to extend the left,
+    // and positive values to cause the shadow to extend to the right.
+    // The default is 0.
+    shadowOffsetX?: number;
+
+    // How far the shadow should extend from the object in the Y direction.
+    // This value isn't affected by the transformation matrix.
+    // Use negative values to cause the shadow to extend up,
+    // and positive values to cause the shadow to extend down.
+    // The default is 0.
+    shadowOffsetY?: number;
+
+    // Indicates the size of the blurring effect;
+    // this value doesn't correspond to a number of pixels
+    // and is not affected by the current transformation matrix.
+    // The default value is 0.
+    shadowBlur?: number;
+
+    // A standard CSS color value indicating the color of the shadow effect;
+    // by default, it is fully-transparent black.
+    shadowColor?: string;
 }
 
 export interface IRect {
